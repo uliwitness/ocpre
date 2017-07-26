e3dc3654f74da4622167f2ad34447c71a276835c
Uli Kusterer <ulivc@zathras.de>
2017-07-26 07:11:27 +0200

    Initial Commit

---
+++ ocpre/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ocpre
+//
+//  Created by Uli Kusterer on 26.07.17.
+//  Copyright © 2017 Uli Kusterer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
